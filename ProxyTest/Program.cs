767c564e9c5fcdea8e8d3f14719b3d9fe4b24f00
Matt Ward <matt.ward@microsoft.com>
2018-10-17 11:48:16 +0100

    Add proxy test console application

---
+++ ProxyTest/Program.cs
@@ -0,0 +1,118 @@
+﻿//
+// Program.cs
+//
+// Author:
+//       Matt Ward <matt.ward@microsoft.com>
+//
+// Copyright (c) 2018 Microsoft
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using AppKit;
+
+namespace ProxyTest
+{
+	class MainClass
+	{
+		static readonly string defaultUrl = "https://www.nuget.org";
+
+		public static void Main (string[] args)
+		{
+			try {
+				NSApplication.Init ();
+				Task task = Run (args);
+				task.Wait ();
+			} catch (Exception ex) {
+				Console.WriteLine (ex.GetBaseException ());
+			}
+		}
+
+		static Task Run (string[] args)
+		{
+			Uri url = GetUrl (args);
+			IWebProxy proxy = GetProxy (url);
+			return Task.Run (() => Connect (url, proxy));
+		}
+
+		static IWebProxy GetProxy (Uri uri)
+		{
+			// Check Proxy types
+			var proxies = CoreFoundation.CFNetwork.GetProxiesForUri (uri, null);
+			if (proxies?.Any () == true) {
+				Console.WriteLine ("Proxy information found:");
+				foreach (var proxy in proxies) {
+					Console.WriteLine ("  ProxyType: {0}", proxy.ProxyType);
+					Console.WriteLine ("  ProxyAddress: '{0}'", proxy.HostName);
+					Console.WriteLine ("  ProxyPort: {0}", proxy.Port);
+					if (proxy.AutoConfigurationUrl != null) {
+						Console.WriteLine ("  ProxyAutoConfigurationUrl: '{0}'", proxy.AutoConfigurationUrl);
+					}
+					if (!string.IsNullOrEmpty (proxy.AutoConfigurationJavaScript)) {
+						Console.WriteLine ("  Proxy has AutoConfigurationJavaScript");
+					}
+					Console.WriteLine ();
+				}
+			}
+
+			// Get the proxy that Mono finds.
+			return ProxyProvider.GetProxy (uri);
+		}
+
+		static Uri GetUrl (string[] args)
+		{
+			if (args.Length == 0)
+				return new Uri (defaultUrl);
+
+			return new Uri (args [0]);
+		}
+
+		static async Task Connect (Uri uri, IWebProxy proxy)
+		{
+			var handler = GetMessageHandler (uri, proxy);
+			var client = new HttpClient (handler);
+			var request = new HttpRequestMessage (HttpMethod.Get, uri);
+
+			Console.WriteLine ("Sending HTTP get to '{0}'", uri);
+			var response = await client.SendAsync (request).ConfigureAwait (false);
+
+			Console.WriteLine ("Response.StatusCode: {0}", response.StatusCode);
+		}
+
+		static HttpMessageHandler GetMessageHandler (Uri uri, IWebProxy proxy)
+		{
+			var handler = new HttpClientHandler ();
+			HttpMessageHandler messageHandler = handler;
+
+			if (proxy != null) {
+				messageHandler = new ProxyAuthenticationHandler (proxy) {
+					InnerHandler = handler
+				};
+				handler.Proxy = proxy;
+				proxy.Credentials = CredentialCache.DefaultCredentials;
+			}
+
+			return messageHandler;
+		}
+	}
+}
---
+++ ProxyTest/ProxyAuthenticationHandler.cs
@@ -0,0 +1,96 @@
+﻿//
+// ProxyAuthenticationHandler.cs
+//
+// Author:
+//       Matt Ward <matt.ward@microsoft.com>
+//
+// Copyright (c) 2018 Microsoft
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProxyTest
+{
+	class ProxyAuthenticationHandler : DelegatingHandler
+	{
+		IWebProxy proxy;
+
+		internal ProxyAuthenticationHandler (IWebProxy proxy)
+		{
+			this.proxy = proxy;
+		}
+
+		protected async override Task<HttpResponseMessage> SendAsync (HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			bool retry = false;
+			while (true) {
+				try {
+					var response = await base.SendAsync (request, cancellationToken);
+
+					if (response.StatusCode != HttpStatusCode.ProxyAuthenticationRequired) {
+						return response;
+					}
+
+					if (retry) {
+						Console.WriteLine ("Proxy credentials rejected for '{0}'", request.RequestUri);
+						return response;
+					}
+
+					if (!AcquireProxyCredentialsAsync (request.RequestUri)) {
+						return response;
+					}
+				} catch (Exception ex)
+				  when (IsProxyAuthenticationRequired (ex)) {
+					if (!AcquireProxyCredentialsAsync (request.RequestUri)) {
+						throw;
+					}
+				}
+
+				retry = true;
+			}
+		}
+
+		static bool IsProxyAuthenticationRequired (Exception ex)
+		{
+			var webException = ex.InnerException as WebException;
+			var response = webException?.Response as HttpWebResponse;
+			return response?.StatusCode == HttpStatusCode.ProxyAuthenticationRequired;
+		}
+
+		bool AcquireProxyCredentialsAsync (Uri url)
+		{
+			Console.WriteLine ("ProxyAuthenticationRequired for '{0}'", url);
+			var credentials = MacProxyCredentialProvider.GetCredentials (url, proxy);
+
+			if (credentials != null) {
+				Console.WriteLine ("Credentials found in KeyChain for for '{0}'", url);
+				proxy.Credentials = credentials;
+				return true;
+			} else {
+				Console.WriteLine ("No credentials found in KeyChain for for '{0}'", url);
+			}
+			return false;
+		}
+	}
+}
